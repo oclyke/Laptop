727a7f6e57932f83e6a5d59f2fc4a50ad45e1439
Andy England <andrew.england@sparkfun.com>
2019-11-07 19:54:21 -0700

    adding audio test

--- Firmware/AudioTest/AudioTest.ino
+++ Firmware/AudioTest/AudioTest.ino
@@ -31,108 +31,133 @@
 #include "arduinoFFT.h"
 #include <FastLED.h>
 
+#define MIC true
+#define JACK false
+
 #define MIC_PIN 34
 #define TIP_PIN A1
 #define RING_PIN A0
 
 #define DATA_PIN 14
 
-#define X_LEDS 15
-#define Y_LEDS 15
-#define NUM_LEDS X_LEDS * Y_LEDS
+#define X_LEDS 21
+#define Y_LEDS 13
+#define MISSING_LEDS 16
+#define NUM_LEDS (X_LEDS * Y_LEDS) - MISSING_LEDS
 
 CRGB leds[NUM_LEDS];
 
-arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
+int ledArray[Y_LEDS][X_LEDS] = {
+  //       0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20
+  /*0*/  { -1,  0, -1, -1, -1, -1, -1, -1, -1,  1,  2, -1, -1, -1, -1, -1, -1, -1, -1,  3, -1},
+  /*1*/  { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  4,  5, -1, -1, -1, -1, -1, -1, -1, -1,  6},
+  /*2*/  { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  7, -1,  8,  9, -1, -1, -1, -1, -1, -1, -1},
+  /*3*/  { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 10, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+  /*4*/  { -1, -1, -1, -1, -1, 11, -1, -1, -1, -1, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+  /*5*/  { -1, -1, -1, -1, -1, -1, -1, 13, -1, -1, 14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+  /*6*/  { 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35},
+  /*7*/  { -1, -1, -1, -1, -1, -1, -1, -1, -1, 36, 37, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+  /*8*/  { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 38, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+  /*9*/  { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 39, 40, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+  /*10*/ { -1, 41, -1, -1, -1, -1, -1, -1, -1, -1, 42, -1, -1, 43, -1, -1, -1, -1, -1, -1, -1},
+  /*11*/ { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 44, -1, -1, -1, 45, -1, -1, -1, -1, -1, 46},
+  /*12*/ { -1, -1, -1, -1, -1, 47, -1, -1, -1, -1, 48, -1, -1, -1, -1, -1, -1, -1, -1, 49, -1}
+};
+
+arduinoFFT FFT = arduinoFFT();
 /*
-These values can be changed in order to evaluate the functions
+  These are the input and output vectors
+  Input vectors receive computed results from FFT
 */
-const uint16_t samples = 256; //This value MUST ALWAYS be a power of 2
-const double signalFrequency = 1000;
-const double samplingFrequency = 5000;
-const uint8_t amplitude = 100;
-/*
-These are the input and output vectors
-Input vectors receive computed results from FFT
-*/
-double vReal[samples];
-double vImag[samples];
-double vReal1[samples];
-double vImag1[samples];
-double vReal2[samples];
-double vImag2[samples];
+#define MAX_SAMPLES 2048
+double vReal[MAX_SAMPLES];
+double vImag[MAX_SAMPLES];
+double vReal1[MAX_SAMPLES];
+double vImag1[MAX_SAMPLES];
 
 #define SCL_INDEX 0x00
 #define SCL_TIME 0x01
 #define SCL_FREQUENCY 0x02
 #define SCL_PLOT 0x03
 
+/*fadeAll - used to fade leds
+   scale - percentage by which leds are faded every step
+*/
+void fadeAll(uint8_t scale = 250)
+{
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i].nscale8(scale);
+  }
+}
+
+/*ComputeFFT - computes our fourier transform
+  bool mic - true if we are reading the mic, false if reading the 3.5mm jack
+  uint16_t sample - number of samples taken to compute FFT, must be a power of 2
+*/
+
+void computeFFT (bool mic, uint16_t samples = 512)
+{
+  if (mic)
+  {
+    for (uint16_t i = 0; i < samples; i++)
+    {
+      vReal[i] = map(analogRead(MIC_PIN), 1100, 2500, -127, 127);
+      vImag[i] = 0.0;
+    }
+    FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD); /* Weigh data */
+    FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
+    FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
+    for (uint16_t i = 0; i < samples; i++)
+    {
+      constrain(vReal[i], 0, 255);
+    }
+  }
+  else
+  {
+    for (uint16_t i = 0; i < samples; i++)
+    {
+      vReal[i] = map(analogRead(TIP_PIN), 2250, 5250, -127, 127);/* Build data with positive and negative values*/
+      vReal1[i] = map(analogRead(RING_PIN), 2250, 5250, -127, 127);
+      vImag[i] = 0.0;
+      vImag1[i] = 0.0;
+    }
+    FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD); /* Weigh data */
+    FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
+    FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
+    FFT.Windowing(vReal1, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD); /* Weigh data */
+    FFT.Compute(vReal1, vImag1, samples, FFT_FORWARD); /* Compute FFT */
+    FFT.ComplexToMagnitude(vReal1, vImag1, samples); /* Compute magnitudes */
+    for (uint16_t i = 0; i < samples; i++)
+    {
+      constrain(vReal[i], 0, 255);
+      constrain(vReal1[i], 0, 255);
+    }
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
   LEDS.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(100);
 }
 
 void loop()
 {
-  /* Build raw data */
-  double cycles = (((samples-1) * signalFrequency) / samplingFrequency); //Number of signal cycles that the sampling will read
-  for (uint16_t i = 0; i < samples; i++)
+  computeFFT(MIC);
+  for (uint8_t x = 0; x < X_LEDS; x++)
   {
-    vReal[i] = map(analogRead(TIP_PIN), 2250, 5250, -127, 127);/* Build data with positive and negative values*/
-    vReal1[i] = map(analogRead(MIC_PIN), 1100, 2500, -127, 127);
-    vReal2[i] = map(analogRead(RING_PIN), 2250, 5250, -127, 127);
-    vImag[i] = 0.0;
-    vImag1[i] = 0.0;
-    vImag2[i] = 0.0;
+    for (uint8_t y = 0; y < Y_LEDS; y++)
+    {
+      CRGB myColor = CHSV(vReal[x + 6], 255 - vReal[x + 6] / 2, vReal[x + 6]);
+      if (myColor.getLuma() > leds[ledArray[y][x]].getLuma())
+      {
+        leds[ledArray[y][x]] = myColor;
+      }
+    }
   }
-  /* Print the results of the simulated sampling according to time */
-  FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD); /* Weigh data */
-  FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
-  FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
-  FFT.Windowing(vReal1, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD); /* Weigh data */
-  FFT.Compute(vReal1, vImag1, samples, FFT_FORWARD); /* Compute FFT */
-  FFT.ComplexToMagnitude(vReal1, vImag1, samples); /* Compute magnitudes */
-  FFT.Windowing(vReal2, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD); /* Weigh data */
-  FFT.Compute(vReal2, vImag2, samples, FFT_FORWARD); /* Compute FFT */
-  FFT.ComplexToMagnitude(vReal2, vImag2, samples); /* Compute magnitudes */
-  for (int led = 0; led < 30; led++)
-  {
-    leds[led * 3] = CHSV(vReal1[led + 2] / 2, 255 - (vReal1[led + 2] / 4), vReal1[led + 2] / 2 );
-    leds[(led * 3) + 1] = CHSV(vReal1[led + 2] / 2, 255 - (vReal1[led + 2] / 4), vReal1[led + 2] / 2);
-    leds[(led * 3) + 2] = CHSV(vReal1[led + 2] / 2 , 255 - (vReal1[led + 2] / 4), vReal1[led + 2] / 2);
-  }
-  /*for (int led = 13; led < 25; led++)
-  {
-    leds[led] = CHSV(vReal1[4], 255 - vReal1[10], vReal1[4] / 2);
-  }*/
+  fadeAll(230);
   FastLED.show();
   //while(1); /* Run Once */
-}
-
-void PrintVector(double *vData, uint16_t bufferSize, uint8_t scaleType)
-{
-  for (uint16_t i = 0; i < bufferSize; i++)
-  {
-    double abscissa;
-    /* Print abscissa value */
-    switch (scaleType)
-    {
-      case SCL_INDEX:
-        abscissa = (i * 1.0);
-	break;
-      case SCL_TIME:
-        abscissa = ((i * 1.0) / samplingFrequency);
-	break;
-      case SCL_FREQUENCY:
-        abscissa = ((i * 1.0 * samplingFrequency) / samples);
-	break;
-    }
-    Serial.print(abscissa, 6);
-    if(scaleType==SCL_FREQUENCY)
-      Serial.print("Hz");
-    Serial.print(" ");
-    Serial.println(vData[i], 4);
-  }
-  Serial.println();
 }

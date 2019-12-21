cadd36cf6e62351cf2fe79a9d6586845371eb472
oclyke <owen.lyke@sparkfun.com>
2019-12-21 01:21:08 -0700

    switch to BLE interface for serial
    drop-in replacement for the old "BluetoothSerial.h"
    most commonly used functions are implemented, more could be added later if needed
    tested very well for typical length transmissions
    poor test setup may have caused/contributed to dropped characters after ~231 bytes, may need additional investigation

---
+++ Firmware/main/BLESerial.cpp
@@ -0,0 +1,115 @@
+/*
+Copyright (c) 2019 Owen Lyke
+Header: BLESerial.h
+*/
+
+#include "BLESerial.h"
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+
+void NUSCallbacks::onWrite(BLECharacteristic *pCharacteristic) {
+  std::string rxValue = pCharacteristic->getValue();
+  if(_p_bleserial != NULL){
+    if (rxValue.length() > 0) {
+      for (int i = 0; i < rxValue.length(); i++){
+        _p_bleserial->_rx_buffer.store_char(rxValue[i]);
+      }
+    }
+  }
+}
+
+
+//
+// Constructor
+BLESerial::BLESerial(void){
+
+}
+
+//
+// Destructor
+BLESerial::~BLESerial(void){
+
+}
+
+//
+// begin
+bool BLESerial::begin(const char* localName="UART Service"){
+  // Create the BLE Device
+  BLEDevice::init(localName);
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new BLESerialServerCallbacks());
+
+  // Create the BLE Service
+  pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pTxCharacteristic = pService->createCharacteristic(
+										CHARACTERISTIC_UUID_TX,
+										BLECharacteristic::PROPERTY_NOTIFY
+									);
+  pTxCharacteristic->addDescriptor(new BLE2902());
+
+  pRxCharacteristic = pService->createCharacteristic(
+											 CHARACTERISTIC_UUID_RX,
+											BLECharacteristic::PROPERTY_WRITE
+										);
+  pRxCharacteristic->setCallbacks(new NUSCallbacks(this));
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  return true;
+}
+
+
+int BLESerial::available(void){
+  return _rx_buffer.available();
+}
+
+int BLESerial::peek(void){
+  return _rx_buffer.peek();
+}
+
+bool BLESerial::hasClient(void){
+  // Serial.println("BLESerial: Not Implemented");
+}
+
+int BLESerial::read(void){
+  return _rx_buffer.read_char();
+}
+
+size_t BLESerial::write(uint8_t c){
+  return write(&c, 1);
+}
+
+size_t BLESerial::write(const uint8_t *buffer, size_t size){
+  size_t remaining = size;
+  size_t sent = 0;
+  while( remaining > ESP_GATT_MAX_ATTR_LEN){
+    pTxCharacteristic->setValue((uint8_t*)(buffer + sent), ESP_GATT_MAX_ATTR_LEN);
+    pTxCharacteristic->notify();
+    remaining -= ESP_GATT_MAX_ATTR_LEN;
+    sent += ESP_GATT_MAX_ATTR_LEN;
+  }
+  pTxCharacteristic->setValue((uint8_t*)(buffer + sent), remaining);
+  pTxCharacteristic->notify();
+}
+
+void BLESerial::flush(){
+  while(read() >= 0){};
+}
+
+void BLESerial::end(void){
+  // Serial.println("BLESerial: Not Implemented");
+}
+
+// esp_err_t register_callback(esp_spp_cb_t * callback){
+//   Serial.println("BLESerial: Not Implemented");
+// }
